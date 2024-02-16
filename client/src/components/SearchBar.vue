07d2b58aa88a11a2edb8f3e2705f7d8dbb6d6344
Alec Tatton <alectatton@Alecs-MacBook-Pro.local>
2024-02-16 15:34:19 -0700

    Implement vuelidate validation

--- client/src/components/SearchBar.vue
+++ client/src/components/SearchBar.vue
@@ -1,49 +1,51 @@
 <template>
     <hr />
-    <Form @submit.prevent="onSubmit">
         <div class="flex justify-center items-center py-4">
-            <div class="flex items-center border-2 border-gray-100 p-1 m-2 space-x-3 bg-white rounded-lg shadow-xl">
-                <span class="material-symbols-outlined pl-2 text-gray-400">
-                    location_on
-                </span>
+                <div class="flex items-center border-2 border-gray-100 p-1 m-2 space-x-3 bg-white rounded-lg shadow-xl">
+                    <span class="material-symbols-outlined pl-2 text-gray-400">
+                        location_on
+                    </span>
 
-                <input 
-                    placeholder="Leaving From"
-                    class="items-center py-1 px-2 rounded-lg focus:outline-none w-40"
-                    type="text"
-                />
-            </div>
+                    <input 
+                        v-model="leaving_from"
+                        placeholder="Leaving From"
+                        class="items-center py-1 px-2 rounded-lg focus:outline-none w-50"
+                        type="text"
+                    />
+                </div>
 
-            <div class="flex items-center border-2 border-gray-100 p-1 m-2 space-x-3 bg-white rounded-lg shadow-xl">
-                <span class="material-symbols-outlined pl-2 text-gray-400">
-                    location_on
-                </span>
+                <div class="flex items-center border-2 border-gray-100 p-1 m-2 space-x-3 bg-white rounded-lg shadow-xl">
+                    <span class="material-symbols-outlined pl-2 text-gray-400">
+                        location_on
+                    </span>
 
-                <input 
-                    placeholder="Going To"
-                    class="items-center py-1 px-2 rounded-lg focus:outline-none w-40"
-                    type="text"
-                />
-            </div>
+                    <input
+                        v-model="going_to"
+                        placeholder="Going To"
+                        class="items-center py-1 px-2 rounded-lg focus:outline-none w-50"
+                        type="text"
+                    />
+                </div>
 
-            <div class="flex items-center border-2 border-gray-100 p-1 m-2 space-x-3 bg-white rounded-lg shadow-xl">
-                <span class="material-symbols-outlined pl-2 text-gray-400">
-                    calendar_today
-                </span>
+                <div class="flex items-center border-2 border-gray-100 p-1 m-2 space-x-3 bg-white rounded-lg shadow-xl">
+                    <span class="material-symbols-outlined pl-2 text-gray-400">
+                        calendar_today
+                    </span>
 
-                <VueDatePicker 
-                    range 
-                    :enable-time-picker="false" 
-                    :preview-format="null"
-                    :min-date="new Date()"
-                    no-today> 
-                    <template #trigger>
-                        <input placeholder="Dates"
-                            class="items-center py-1 px-2 rounded-lg focus:outline-none w-40"
-                        />
-                    </template>
-                </VueDatePicker>
-            </div>
+                    <VueDatePicker
+                        v-model="date_range"
+                        range 
+                        :enable-time-picker="false" 
+                        :preview-format="null"
+                        :min-date="new Date()"
+                        no-today> 
+                        <template #trigger>
+                            <input :placeholder="date_range ? date_range : 'Dates'"
+                                class="items-center py-1 px-2 rounded-lg focus:outline-none w-50"
+                            />
+                        </template>
+                    </VueDatePicker>
+                </div>
 
             <div class="flex items-center border-2 border-gray-100 p-1 m-2 space-x-3 bg-white rounded-lg shadow-xl">
                 <span class="material-symbols-outlined pl-2 text-gray-400">
@@ -51,39 +53,72 @@
                 </span>
 
                 <input 
+                    v-model="travelers"
                     placeholder="Travelers"
-                    class="items-center py-1 px-2 rounded-lg focus:outline-none w-40"
+                    class="items-center py-1 px-2 rounded-lg focus:outline-none w-50"
                     type="text"
                 />
             </div>
 
             <button 
                 type="submit"
+                @click.prevent="onSubmit"
                 class="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition duration-300">
                 Search
             </button>
         </div>
-    </Form>
     <hr />
 </template>
   
 <script lang="ts">
 import VueDatePicker from '@vuepic/vue-datepicker';
-import { defineRule, Form, Field, ErrorMessage } from 'vee-validate';
-import { ValidationProvider, extend } from 'vee-validate';
-import { required } from '@vee-validate/rules';
+import { useVuelidate } from '@vuelidate/core';
+import { required, integer, minValue } from '@vuelidate/validators';
 
 export default {
     name: 'SearchBar',
+    setup () {
+        return { v$: useVuelidate()}
+    },
     components: {
         VueDatePicker,
-        Form,
-        Field,
-        ErrorMessage,
     },
     methods: {
-        onSubmit() {
+        async onSubmit() {
+            const isValid = await this.v$.$validate();
+
+            if (!isValid) {
+                console.log('Form is invalid');
+                return;
+            }
+
             console.log('Form submitted');
+        }
+    },
+    data () {
+        return {
+            leaving_from: '',
+            going_to: '',
+            date_range: '',
+            travelers: ''
+        }
+    },
+    validations () {
+        return {
+            leaving_from: {
+                required
+            },
+            going_to: {
+                required
+            },
+            date_range: {
+                required
+            },
+            travelers: {
+                required,
+                integer,
+                minValue: minValue(1)
+            }
         }
     }
 }
